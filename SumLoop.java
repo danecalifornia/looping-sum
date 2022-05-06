6d0fdf76fc70aad296c3ace397756c9f06e4448a
danecalifornia <69050884+danecalifornia@users.noreply.github.com>
2022-05-06 17:06:16 -0500

    Add files via upload

---
+++ SumLoop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumLoop {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int number1, number2;
+        char choice;
+
+        do {
+            System.out.print("Enter the first number: ");
+            number1 = console.nextInt();
+
+            System.out.print("Enter the second number: ");
+            number2 = console.nextInt();
+
+            int sum = number1 + number2;
+            System.out.println("Sum of numbers: " + sum);
+
+            System.out.print("Do you want to continue? Type Y or N. ");
+            choice = console.next().charAt(0);
+
+            System.out.println();
+
+        } while(choice == 'y' || choice == 'Y');
+    }
+}
